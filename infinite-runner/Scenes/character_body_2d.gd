6599c38d3603c431699955a3d0f5fd708536fdd5
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2025-07-20 21:46:15 +1200

    feat(game): player infinitely runs

--- infinite-runner/Scenes/character_body_2d.gd
+++ infinite-runner/Scenes/character_body_2d.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var INITIAL_SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +11,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
+	#TODO: make jump and slide actions through swipe controls
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	#infinite run
+	#TODO: make speed up over time for difficulty
+	velocity.x = 1 * INITIAL_SPEED
 
 	move_and_slide()
