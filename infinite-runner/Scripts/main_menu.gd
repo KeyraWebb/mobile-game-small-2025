d03dbc59eeddc2fc9e6c542f2d86131e86acd56a
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2025-07-22 09:53:46 +1200

    feat(mainmenu): create credits screen

--- infinite-runner/Scripts/main_menu.gd
+++ infinite-runner/Scripts/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 @export var playscene : PackedScene
+@onready var credits = $Credits
 
-func _on_button_pressed() -> void:
+func _on_button_pressed() -> void: #play button
 	get_tree().change_scene_to_packed(playscene)
+
+
+func _on_credits_button_pressed() -> void:
+	credits.visible = true
