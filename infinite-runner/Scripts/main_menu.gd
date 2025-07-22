5a7f981391a41a9c47ee1beba8879d0ce38fc5e3
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2025-07-22 19:19:43 +1200

    feat(pause menu): add functionality to main menu button

--- infinite-runner/Scripts/main_menu.gd
+++ infinite-runner/Scripts/main_menu.gd
@@ -1,9 +1,8 @@
 extends Control
-@export var playscene : PackedScene
 @onready var credits = $Credits
 
 func _on_button_pressed() -> void: #play button
-	get_tree().change_scene_to_packed(playscene)
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
 
 func _on_credits_button_pressed() -> void:
--- infinite-runner/Scripts/pause_menu.gd
+++ infinite-runner/Scripts/pause_menu.gd
@@ -1,5 +1,4 @@
 extends PanelContainer
-@export var mainMenu : PackedScene
 
 func _input(event) -> void:
 	if event.is_action_pressed("Pause"):
@@ -13,5 +12,5 @@ func _on_resume_pressed() -> void:
 	get_tree().paused = !get_tree().paused
 
 func _on_main_menu_pressed() -> void:
-	print(mainMenu)
-	get_tree().change_scene_to_packed(mainMenu)
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
