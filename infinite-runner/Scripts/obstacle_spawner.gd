63b57e9222de8139ec3d479cfaf7ddb3fd7851dc
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2025-07-23 20:29:31 +1200

    feat(game) obstacles now spawn

--- infinite-runner/Scripts/obstacle_spawner.gd
+++ infinite-runner/Scripts/obstacle_spawner.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 @export var frequency : int
+@onready var obstacleHolder = $"../../Obstacle holder"
+@onready var spawnTimer = $"../../Timer"
 var spawnPoints : Array
+var timeElapsed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawnPoints = get_children()
-	print(spawnPoints)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_timer_timeout() -> void:
+	var select = randi_range(0, spawnPoints.size() - 1)
+	spawnPoints[select].spawn(obstacleHolder)
+	spawnTimer.start(1)
+	print("spawn")
--- infinite-runner/Scripts/spawn_point.gd
+++ infinite-runner/Scripts/spawn_point.gd
@@ -2,11 +2,8 @@ extends Node2D
 
 @export var obstacles : Array[PackedScene]
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func spawn(parent):
+	var select = randi_range(0, obstacles.size() - 1)
+	var newObstacle = obstacles[select].instantiate()
+	newObstacle.global_position = self.global_position
+	parent.add_child(newObstacle)
