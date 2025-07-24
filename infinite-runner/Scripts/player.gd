d915382d606e4f60daac987b9698df0e7f0b7c71
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2025-07-25 11:44:26 +1200

    feat(player): start work on slide functionality

--- infinite-runner/Scripts/player.gd
+++ infinite-runner/Scripts/player.gd
@@ -3,16 +3,35 @@ extends CharacterBody2D
 
 @export var INITIAL_SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
+@export var slideLength = 300
+
+@onready var jumpCollider = $"Jump Collider"
+@onready var slideCollider = $"Slide Collider"
 @onready var camera = $Camera2D
+@onready var uprightSprite = $"Upright Sprite"
+@onready var slideSprite = $"Slide Sprite"
+var sliding = false
+var currentSlide = 0
+
 
 func _ready() -> void:
 	camera.swipeUp.connect(_on_swipeup.bind())
 	camera.swipeDown.connect(_on_swipedown.bind())
+	slideCollider.disabled = true
+	jumpCollider.disabled = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		if sliding: #terminate slide
+			DisableSlide()
 		velocity += get_gravity() * delta
+	
+	if sliding: #slide handling
+		if currentSlide > 0:
+			currentSlide - delta
+		else:
+			DisableSlide()
 	
 	#infinite run
 	#TODO: make speed up over time for difficulty
@@ -25,4 +44,24 @@ func _on_swipeup():
 		velocity.y = JUMP_VELOCITY
 		
 func _on_swipedown():
-	pass
+	velocity.y = -1 * JUMP_VELOCITY
+	
+	#switch appearance and collision
+	jumpCollider.disabled = true
+	slideCollider.disabled = false
+	uprightSprite.visible = false
+	slideSprite.visible = true
+	
+	#start slide
+	sliding = true
+	currentSlide = slideLength
+	
+#code for terminating the slide
+func DisableSlide():
+	sliding = false
+	slideCollider.disabled = true
+	jumpCollider.disabled = false
+	uprightSprite.visible = true
+	slideSprite.visible = false
+	
+	
