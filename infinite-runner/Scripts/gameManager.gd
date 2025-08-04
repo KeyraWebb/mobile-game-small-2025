8ee16f0d321d5825fd966ab686f1b38cfd3b04a0
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2025-08-04 18:47:29 +1200

    refactor(game): changed touch input buttons to texture buttons for better screen size handling

--- infinite-runner/Scripts/gameManager.gd
+++ infinite-runner/Scripts/gameManager.gd
@@ -6,8 +6,16 @@ var score = 0
 @onready var upButton = $"CanvasLayer/Up Button"
 @onready var downButton = $"CanvasLayer/Down Button"
 
+var pauseAction = InputEventAction.new()
+var upAction = InputEventAction.new()
+var downAction = InputEventAction.new()
+
 func _ready() -> void:
 	setAccessMode()
+	pauseAction.action = "Pause"
+	pauseAction.pressed = true
+	upAction.action = "Up"
+	downAction.action = "Down"
 
 func _process(delta: float) -> void:
 	score += delta * scoreMagnifier
@@ -16,3 +24,23 @@ func _process(delta: float) -> void:
 func setAccessMode():
 	upButton.visible = Global.AccessMode
 	downButton.visible = Global.AccessMode
+
+#button controls - simulating the touch screen buttons so that they can be anchored
+func _on_menu_button_pressed() -> void:
+	Input.parse_input_event(pauseAction)
+
+func _on_up_button_button_down() -> void:
+	upAction.pressed = true
+	Input.parse_input_event(upAction)
+
+func _on_up_button_button_up() -> void:
+	upAction.pressed = false
+	Input.parse_input_event(upAction)
+
+func _on_down_button_button_down() -> void:
+	downAction.pressed = true
+	Input.parse_input_event(downAction)
+
+func _on_down_button_button_up() -> void:
+	downAction.pressed = false
+	Input.parse_input_event(downAction)
