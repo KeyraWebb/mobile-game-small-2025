780b8af02162c7b9a8fcaf4ce289ea7bf9c9ef11
KeyraWebb <125842819+KeyraWebb@users.noreply.github.com>
2025-10-22 07:20:31 +1300

    refactor(upgrades): tweaked price for life upgrade

--- infinite-runner/Scenes/Objects/Collectables/coin.gd
+++ infinite-runner/Scenes/Objects/Collectables/coin.gd
@@ -2,6 +2,5 @@ extends Area2D
 @onready var collider = $CollisionShape2D
 
 func _on_body_entered(body: Node2D) -> void:
-	print("bye")
 	body.CollectCoin()
 	self.queue_free()
--- infinite-runner/Scripts/upgrades.gd
+++ infinite-runner/Scripts/upgrades.gd
@@ -6,24 +6,29 @@ extends Control
 @onready var coinTotal = $Coins
 
 var currLifeCost
+var lifePrices = [100,500,1000,2000,5000]
 
 func _ready():
 	updateLifeCost()
 
 func _on_exit_pressed() -> void:
-	print("aaaaaa")
 	self.visible = !self.visible
 
 func updateLifeCost():
-	currLifeCost = pow(lifeBaseCost, (Global.Lives + 1))
-	lifeCostText.text = "%0.0f" % currLifeCost
+	if Global.Lives < len(lifePrices):
+		currLifeCost = lifePrices[Global.Lives]
+		lifeCostText.text = "%0.0f" % currLifeCost
+	else:
+		currLifeCost = -1
+		lifeCostText.text = "SOLD OUT"
 
 func _on_life_upgrade_pressed() -> void:
-	if Global.Coins > currLifeCost:
+	if currLifeCost > 0 && Global.Coins > currLifeCost:
 		Global.Coins -= currLifeCost
 		Global.Lives += 1
 		updateLifeCost()
 		updateCoinDisplay()
+		Global.saveGame()
 		
 func updateCoinDisplay():
 	coinTotal.text = str(Global.Coins)
